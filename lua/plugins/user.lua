8239dd889227b2d600a7b2e828d5166a4bd9b991
Yury Kartynnik <kartynnik@google.com>
2025-03-21 15:22:41 +0000

    Switch colorscheme to my version of `janah`

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,4 +1,6 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+if true then return {
+  "kartynnik/vim-janah",
+} end
 
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
