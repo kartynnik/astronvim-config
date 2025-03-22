fb88f43360a49cce9e699cc16eed12c76fd32751
Yury Kartynnik <kartynnik@google.com>
2025-03-22 23:51:34 +0000

    Fix `format_on_save`

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -2,10 +2,10 @@ return {
   "AstroNvim/astrolsp",
   ---@type AstroLSPOpts
   opts = {
-  },
-  formatting = {
-    format_on_save = {
-      enabled = false, -- enable or disable format on save globally
+    formatting = {
+      format_on_save = {
+        enabled = false, -- enable or disable format on save globally
+      },
     },
-  }
+  },
 }
