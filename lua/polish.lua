2f593865fce6684f1970ef5bce4f6d09470caabb
Yury Kartynnik <kartynnik@google.com>
2025-03-23 03:53:39 +0000

    `cmdheight = 1` to avoid "press ENTER" prompts

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,6 +1,7 @@
 vim.o.wildmode = "noselect:full"
 vim.o.autochdir = true
 vim.o.virtualedit = "all"
+vim.o.cmdheight = 1
 
 local work_polish = vim.fs.normalize "~/.work-repo/nvim/polish.lua"
 if vim.uv.fs_stat(work_polish) then loadfile(work_polish)() end
