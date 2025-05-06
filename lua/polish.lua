66c6134abb475e7da8eeba32787e1c109b9a186b
Yury Kartynnik <kartynnik@google.com>
2025-05-06 21:04:42 +0200

    Add angle brackets to `matchpairs`

--- lua/polish.lua
+++ lua/polish.lua
@@ -2,6 +2,7 @@ vim.o.wildmode = "noselect:full"
 vim.o.autochdir = true
 vim.o.virtualedit = "all"
 vim.o.cmdheight = 1
+vim.o.matchpairs = "(:),{:},[:],<:>"
 
 local work_polish = vim.fs.normalize "~/.work-repo/nvim/polish.lua"
 if vim.uv.fs_stat(work_polish) then loadfile(work_polish)() end
